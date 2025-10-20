ae5545fc75c19f52ff39cec4e40ab4f7a2181644
Dudxxx <130068099+Dudxxx@users.noreply.github.com>
2025-10-19 22:51:17 -0300

    ajuste para rodar em qualquer computador, estava com erro na hora do npm i

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react' // ou outro plugin que você use
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer()
+      ]
+    }
+  }
 })
